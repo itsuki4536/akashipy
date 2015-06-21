328c3c262d2cc0c1c1c9bc495601d8e091c10120
itsuki <ituki1024@me.com>
2015-06-21 15:47:01 +0900

    検索するキーワードを指定できるようにした

--- StudyPython.py
+++ StudyPython.py
@@ -1,8 +1,11 @@
 
 import requests
 
+print("検索したいキーワード")
 
-r = requests.get('http://connpass.com/api/v1/event/?keyword=python')
+keyword = input()
+
+r = requests.get('http://connpass.com/api/v1/event/?keyword={0}'.format(keyword))
 print(r.status_code, r.headers['content-type'])
 
 for event in r.json()['events']:
