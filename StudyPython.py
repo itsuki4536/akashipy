c7fb9c02bf5a8526b6c973a85efea15480a9e9b6
itsuki <ituki1024@me.com>
2015-06-21 15:31:24 +0900

    イベントのタイトルと日時を表示した

--- StudyPython.py
+++ StudyPython.py
@@ -1,5 +1,9 @@
 
 import requests
 
+
 r = requests.get('http://connpass.com/api/v1/event/?keyword=python')
 print(r.status_code, r.headers['content-type'])
+
+for event in r.json()['events']:
+    print(event['title'], event['started_at'])
