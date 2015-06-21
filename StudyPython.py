81b2d5bd68bafec026e1a4416cc7141e3b04c441
itsuki <ituki1024@me.com>
2015-06-21 16:06:26 +0900

    実行結果をevents.txtに保存するようにした

--- StudyPython.py
+++ StudyPython.py
@@ -8,5 +8,12 @@ keyword = input()
 r = requests.get('http://connpass.com/api/v1/event/?keyword={0}'.format(keyword))
 print(r.status_code, r.headers['content-type'])
 
+f = open("events.txt","w")
 for event in r.json()['events']:
-    print(event['title'], event['started_at'])
+    s = "{0} {1}".format(event['title'], event['started_at'])
+    print(s)
+    f.write(s + "\n")
+
+f.close()
+
+
